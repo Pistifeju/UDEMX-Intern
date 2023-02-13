056da99e6912d9034f0c974ec06127a8f1ec97ca
Pistifeju <juhasz.pisti7253@gmail.com>
2023-02-13 21:59:50 +0100

    Initial Commit

---
+++ UDEMX-Intern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UDEMX-Intern
+//
+//  Created by István Juhász on 2023. 02. 13..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
