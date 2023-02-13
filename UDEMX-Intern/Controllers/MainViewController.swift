1cefe248bc62e88b9d3bb07fdd9ad4d3d2d814e4
Pistifeju <juhasz.pisti7253@gmail.com>
2023-02-13 22:27:28 +0100

    Created folder structure for MVC design

---
+++ UDEMX-Intern/Controllers/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  UDEMX-Intern
+//
+//  Created by István Juhász on 2023. 02. 13..
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupUI()
+    }
+    
+    // MARK: - Helpers
+    private func setupUI() {
+        self.view.backgroundColor = .systemBlue
+        
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+    
+    // MARK: - Selectors
+}
+
